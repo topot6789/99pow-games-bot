644b3da16cb2bf7284c90dbb97eb7ce54dbb257f
Blinkk Lant <shinnish07@gmail.com>
2026-01-16 23:11:25 +0800

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -13,8 +13,8 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 TARGET_CHAT_ID = -1003539357826
 PH_TZ = pytz.timezone("Asia/Manila")
 
-LOCK_TIME = dtime(hour=23, minute=04)
-UNLOCK_TIME = dtime(hour=23, minute=05)
+LOCK_TIME = dtime(hour=23, minute=13)
+UNLOCK_TIME = dtime(hour=23, minute=14)
 
 LOCKED_PERMISSIONS = ChatPermissions(
     can_send_messages=False
